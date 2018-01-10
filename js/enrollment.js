652e8e8688f7bb01245e33aed2b9be8f6a8fd8d6
itigoppo <itigoppo+github@gmail.com>
2018-01-10 19:04:17 +0900

    メンバー追加やら

--- js/enrollment.js
+++ js/enrollment.js
@@ -5,31 +5,69 @@ $(document).ready(function () {
     var today_day = today.getDate();
     var today_string = zeroPadding(today_year, 4) + zeroPadding(today_month, 2) + zeroPadding(today_day, 2);
 
+    var column_number_name = 1;
+    var column_number_birth_date = 2;
+    var column_number_age = 3;
+    var column_number_join_date = 4;
+    var column_number_join_age = 5;
+    var column_number_graduate_date = 6;
+    var column_number_graduate_age = 7;
+    var column_number_enrollment = 8;
+    var column_number_enrollment_day = 9;
+    var column_number_unit = 10;
+    var column_number_unit_graduate_date = 11;
+    var column_number_unit_graduate_age = 12;
+    var column_number_unit_enrollment = 13;
+    var column_number_unit_enrollment_day = 14;
+    var column_number_concurrent_unit = 15;
+    var column_number_concurrent_join_date = 16;
+    var column_number_concurrent_join_age = 17;
+    var column_number_concurrent_graduate_date = 18;
+    var column_number_concurrent_graduate_age = 18;
+    var column_number_concurrent_enrollment = 19;
+    var column_number_concurrent_enrollment_day = 20;
+
     $('#member tbody tr').each(function (index, element) {
-        var birth_date = new Date($(element).children('td:nth-child(2)').text());
+        // テーブルのデータ取得
+        var input_birth = $(element).children('td:nth-child(' + column_number_birth_date + ')').text();
+        var input_join = $(element).children('td:nth-child(' + column_number_join_date + ')').text();
+        var input_graduate = $(element).children('td:nth-child(' + column_number_graduate_date + ')').text();
+        var input_unit = $(element).children('td:nth-child(' + column_number_unit + ')').text();
+        var input_unit_graduate = $(element).children('td:nth-child(' + column_number_unit_graduate_date + ')').text();
+        var input_concurrent_join = $(element).children('td:nth-child(' + column_number_concurrent_join_date + ')').text();
+        var input_concurrent_graduate = $(element).children('td:nth-child(' + column_number_concurrent_graduate_date + ')').text();
+
+
+        var birth_date = new Date(input_birth);
         var birth_year = birth_date.getFullYear();
         var birth_month = birth_date.getMonth() + 1;
         var birth_day = birth_date.getDate();
         var birth_string = zeroPadding(birth_year, 4) + zeroPadding(birth_month, 2) + zeroPadding(birth_day, 2);
 
-        var join_date = new Date($(element).children('td:nth-child(4)').text());
+        var join_date = today;
+        if (input_join.indexOf('dd') !== -1) {
+            var join_day_unknown = input_join.split('/');
+            join_date = new Date(Number(join_day_unknown[0]), Number(join_day_unknown[1]) + 1, 0);
+        } else {
+            join_date = new Date(input_join);
+        }
         var join_year = join_date.getFullYear();
         var join_month = join_date.getMonth() + 1;
         var join_day = join_date.getDate();
         var join_string = zeroPadding(join_year, 4) + zeroPadding(join_month, 2) + zeroPadding(join_day, 2);
 
-        var input_graduate = $(element).children('td:nth-child(6)').text();
-        var graduate_date = today;
-        if (input_graduate) {
-            graduate_date = new Date(input_graduate);
+        var unit_graduate_date = today;
+        if (input_unit_graduate.indexOf('dd') !== -1) {
+            var unit_graduate_day_unknown = input_unit_graduate.split('/');
+            unit_graduate_date = new Date(Number(unit_graduate_day_unknown[0]), Number(unit_graduate_day_unknown[1]) + 1, 0);
+        } else if(input_unit_graduate) {
+            unit_graduate_date = new Date(input_unit_graduate);
         }
-        var graduate_year = graduate_date.getFullYear();
-        var graduate_month = graduate_date.getMonth() + 1;
-        var graduate_day = graduate_date.getDate();
-        var graduate_string = zeroPadding(graduate_year, 4) + zeroPadding(graduate_month, 2) + zeroPadding(graduate_day, 2);
+        var unit_graduate_year = unit_graduate_date.getFullYear();
+        var unit_graduate_month = unit_graduate_date.getMonth() + 1;
+        var unit_graduate_day = unit_graduate_date.getDate();
+        var unit_graduate_string = zeroPadding(unit_graduate_year, 4) + zeroPadding(unit_graduate_month, 2) + zeroPadding(unit_graduate_day, 2);
 
-
-        var input_concurrent_join = $(element).children('td:nth-child(10)').text();
         var concurrent_join_date = today;
         if (input_concurrent_join) {
             concurrent_join_date = new Date(input_concurrent_join);
@@ -39,7 +77,6 @@ $(document).ready(function () {
         var concurrent_join_day = concurrent_join_date.getDate();
         var concurrent_join_string = zeroPadding(concurrent_join_year, 4) + zeroPadding(concurrent_join_month, 2) + zeroPadding(concurrent_join_day, 2);
 
-        var input_concurrent_graduate = $(element).children('td:nth-child(12)').text();
         var concurrent_graduate_date = today;
         if (input_concurrent_graduate) {
             concurrent_graduate_date = new Date(input_concurrent_graduate);
@@ -49,45 +86,72 @@ $(document).ready(function () {
         var concurrent_graduate_day = concurrent_graduate_date.getDate();
         var concurrent_graduate_string = zeroPadding(concurrent_graduate_year, 4) + zeroPadding(concurrent_graduate_month, 2) + zeroPadding(concurrent_graduate_day, 2);
 
+        var graduate_date = today;
+        if (input_graduate.indexOf('dd') !== -1) {
+            var graduate_day_unknown = input_graduate.split('/');
+            graduate_date = new Date(Number(graduate_day_unknown[0]), Number(graduate_day_unknown[1]) + 1, 0);
+        } else if(input_graduate) {
+            graduate_date = new Date(input_graduate);
+        }
+        var graduate_year = graduate_date.getFullYear();
+        var graduate_month = graduate_date.getMonth() + 1;
+        var graduate_day = graduate_date.getDate();
+        var graduate_string = zeroPadding(graduate_year, 4) + zeroPadding(graduate_month, 2) + zeroPadding(graduate_day, 2);
+
         // 年齢
         var age = Math.floor((Number(today_string) - Number(birth_string)) / 10000);
-        $(element).children('td:nth-child(3)').text(age);
+        $(element).children('td:nth-child(' + column_number_age +')').text(age);
 
         // 加入時年齢
         var join_age = Math.floor((Number(join_string) - Number(birth_string)) / 10000);
-        $(element).children('td:nth-child(5)').text(join_age);
+        $(element).children('td:nth-child(' + column_number_join_age + ')').text(join_age);
 
-        // 卒業時年齢
+        // H!P卒業時年齢
         if (input_graduate) {
             var graduate_age = Math.floor((Number(graduate_string) - Number(birth_string)) / 10000);
-            $(element).children('td:nth-child(7)').text(graduate_age);
+            $(element).children('td:nth-child(' + column_number_graduate_age + ')').text(graduate_age);
         } else {
             $(element).addClass('bg-success');
+            $(element).hide();
         }
 
-        // 在籍期間
+        // H!P在籍期間
         var graduate_diff = graduate_date.getTime() - join_date.getTime();
         var enrollment_day = Math.floor(graduate_diff / 1000 / 60 / 60 / 24);
-        var enrollment_year = Math.floor(enrollment_day / 1000 / 60 / 60 / 24);
+        $(element).children('td:nth-child(' + column_number_enrollment_day + ')').text(enrollment_day);
 
+        // ユニット卒業時年齢
+        if (input_unit_graduate) {
+            var unit_graduate_age = Math.floor((Number(unit_graduate_string) - Number(birth_string)) / 10000);
+            $(element).children('td:nth-child(' + column_number_unit_graduate_age + ')').text(unit_graduate_age);
+        }
 
-        // var birth_diff_day = Math.floor(birth_diff / 1000 / 60 / 60 / 24);
-        // var birth_diff_year = birth_diff / 1000 * 60 * 60 * 24 * 365;
+        // ユニット在籍期間
+        var unit_graduate_diff = unit_graduate_date.getTime() - join_date.getTime();
+        var unit_enrollment_day = Math.floor(unit_graduate_diff / 1000 / 60 / 60 / 24);
+        var enrollment_year = Math.floor(unit_enrollment_day / 1000 / 60 / 60 / 24);
 
-        $(element).children('td:nth-child(9)').text(enrollment_day);
-
-
+        if (input_unit !== 'ソロ') {
+            $(element).children('td:nth-child(' + column_number_unit_enrollment_day + ')').text(unit_enrollment_day);
+        }
 
         // 兼任先加入時年齢
         if (input_concurrent_join) {
             var concurrent_join_age = Math.floor((Number(concurrent_join_string) - Number(birth_string)) / 10000);
-            $(element).children('td:nth-child(11)').text(concurrent_join_age);
+            $(element).children('td:nth-child(' + column_number_concurrent_join_age + ')').text(concurrent_join_age);
         }
 
         // 兼任先卒業時年齢
         if (input_concurrent_graduate) {
             var concurrent_graduate_age = Math.floor((Number(concurrent_graduate_string) - Number(birth_string)) / 10000);
-            $(element).children('td:nth-child(13)').text(concurrent_graduate_age);
+            $(element).children('td:nth-child(' + column_number_concurrent_graduate_age + ')').text(concurrent_graduate_age);
+        }
+
+        // 兼任先在籍期間
+        if (input_concurrent_join) {
+            var concurrent_graduate_diff = concurrent_graduate_date.getTime() - concurrent_join_date.getTime();
+            var concurrent_enrollment_day = Math.floor(concurrent_graduate_diff / 1000 / 60 / 60 / 24);
+            $(element).children('td:nth-child(' + column_number_concurrent_enrollment_day + ')').text(concurrent_enrollment_day);
         }
     });
 });
